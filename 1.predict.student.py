aa86694ce66ec9c4264773c7f6b96505d495321f
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-06-06 12:38:56 +0600

    core skeleton is complite

---
+++ 1.predict.student.py
@@ -0,0 +1,21 @@
+import joblib
+
+# Load Save Model
+model = joblib.load("student_placement_model.pkl")
+
+print("Model loaded!")
+
+# Predict new student placement
+result = model.predict([[
+    107,
+    6.61,
+    6.28,
+    8,
+    0,
+    8,
+    8,
+    4,
+]])
+
+print("Prediction:", result)
+
